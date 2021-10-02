5109cf15ea11c2736543801fda13d8d564d482b6
mbul48 <45551081+mbul48@users.noreply.github.com>
2021-10-03 02:09:13 +0700

    Update AngkaRibuan.java
    add penjumlahan

--- AngkaRibuan.java
+++ AngkaRibuan.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Locale bahasa = new Locale("id");
-    Scanner inp = new Scanner(System.in);
-    int harga = inp.nextInt();
+    Scanner input = new Scanner(System.in);
+    int harga = input.nextInt();
+    int a = 80;
+    int b = 90;
+    
+    System.out.println(a+b);
     
     System.out.printf(bahasa, "Rp%,d,-", harga);
     
